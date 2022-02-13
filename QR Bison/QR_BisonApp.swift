e7e3fa07a79e51af0e35300301afaf95f0e2468b
Kate <kate.lorenzo@gallaudet.edu>
2022-02-13 16:33:57 -0500

    Initial Commit

---
+++ QR Bison/QR_BisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_BisonApp.swift
+//  QR Bison
+//
+//  Created by Kate Lorenzo on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct QR_BisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
