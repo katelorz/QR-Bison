c9cedc7d6282c42e328a9a1bd4e6122c90d1b3f8
Kate <kate.lorenzo@gallaudet.edu>
2022-02-18 17:46:37 -0500

    hi

--- QR Bison/ContentView.swift
+++ QR Bison/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, Dr. Raja Kushalnagar!")
-            .padding()
+        VStack {
+            //NavigationView{
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 100)
+                
+                Text("QR Bison")
+                    .font(.system(size: 70))
+                    .padding()
+                
+                Spacer()
+                
+                VStack(alignment: .leading){
+                    Text("Username: ")
+                    TextField(
+                        "",
+                        text: $username
+                    )
+                        .disableAutocorrection(true)
+                }
+                .padding(.leading, 30)
+                .padding(.trailing, 30)
+                
+                VStack(alignment: .leading) {
+                    Text("Password: ")
+                    TextField(
+                        "",
+                        text: $password
+                    )
+                        .disableAutocorrection(true)
+                }
+                .padding(.leading, 30)
+                .padding(.trailing, 30)
+                .padding(.bottom, 30)
+                
+                Button(
+                    action: { }, label: {Text("Log In")}
+                )
+                    .foregroundColor(Color.white)
+                    .frame(width: 310)
+                    .padding(10)
+                    .background(.blue)
+                    .cornerRadius(10)
+                
+                Link("Forgot password?", destination: URL(string: "https://ssl.gallaudet.edu/passwordassistance/")!)
+                
+                Spacer()
+                Spacer()
+            }
+            .textFieldStyle(.roundedBorder)
+        //}
     }
 }
 
---
+++ QR Bison/Homepage.swift
@@ -0,0 +1,20 @@
+//
+//  Homepage.swift
+//  QR Bison
+//
+//  Created by Kate Lorenzo on 2/18/22.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Homepage_Previews: PreviewProvider {
+    static var previews: some View {
+        Homepage()
+    }
+}
