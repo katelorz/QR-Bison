e3c714fa6e1dc32719de7784cd6c2a3c3eec5874
VillainWaldo <89269288+VillainWaldo@users.noreply.github.com>
2022-03-04 10:33:43 -0500

    Add files via upload

---
+++ qrbison/qrbisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrbisonApp.swift
+//  qrbison
+//
+//  Created by Vladimir Slavov on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct qrbisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
